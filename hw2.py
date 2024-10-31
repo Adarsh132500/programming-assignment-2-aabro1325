0af6b232c9f242edae3098ac4aeeadc99932410e
Adarsh132500 <admuruge@calpoly.edu>
2024-10-30 21:22:41 -0700

    programming assignment

--- hw2.py
+++ hw2.py
@@ -1,20 +1,67 @@
 import data
+from data import Point, Duration
+from data import Rectangle
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(p1:Point, p2:Point) -> Rectangle:
+    if p1.y<p2.y:
+        p1.y, p2.y = p2.y, p1.y
+    return Rectangle(p1, p2)
 
 # Part 2
-
+def shorter_duration_than(dur1:Duration, dur2:Duration) -> bool:
+    if dur1.minutes>dur2.minutes:
+        return False
+    elif dur1.minutes<dur2.minutes:
+        return True
+    else:
+        if dur1.seconds > dur2.seconds:
+            return False
+        elif dur1.seconds < dur2.seconds:
+            return True
 
 # Part 3
-
+def songs_shorter_than(lst, dur:Duration):
+    return [x for x in lst if shorter_duration_than(x.duration, dur)]
 
 # Part 4
-
+def running_time(songs, ints):
+    dur = Duration(0,0)
+    for x in ints:
+        dur.minutes += songs[x].duration.minutes
+        dur.seconds += songs[x].duration.seconds
+    dur.minutes += dur.seconds//60
+    dur.seconds = dur.seconds%60
+    return dur
 
 # Part 5
-
+def validate_route(clist, route):
+    count = 0
+    for x in clist:
+        if route[count] in x:
+            if route[count+1] in x:
+                count+=1
+                if (count)==len(route)-1: return True
+    return False
 
 # Part 6
+def longest_repetition(numbers):
+    if not numbers:
+        return None
+    max_len = current_len = 1
+    max_index = current_index = 0
+    for i in range(1, len(numbers)):
+        if numbers[i] == numbers[i - 1]:
+            current_len += 1
+        else:
+            if current_len > max_len:
+                max_len = current_len
+                max_index = current_index
+            current_len = 1
+            current_index = i
+    if current_len > max_len:
+        max_len = current_len
+        max_index = current_index
+    return max_index
--- hw2_tests.py
+++ hw2_tests.py
@@ -1,4 +1,5 @@
 import data
+from data import Point, Rectangle, Duration, Song
 import hw2
 import unittest
 
@@ -7,23 +8,105 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_create_rectangle_case_1(self):
+        p1 = Point(2, 2)
+        p2 = Point(10, 10)
+        rect = hw2.create_rectangle(p1, p2)
+        self.assertEqual(rect.top_left.x, 2)
+        self.assertEqual(rect.top_left.y, 10)
+        self.assertEqual(rect.bottom_right.x, 10)
+        self.assertEqual(rect.bottom_right.y, 2)
 
+    def test_create_rectangle_case_2(self):
+        p1 = Point(5, 5)
+        p2 = Point(5, 10)
+        rect = hw2.create_rectangle(p1, p2)
+        self.assertEqual(rect.top_left.x, 5)
+        self.assertEqual(rect.top_left.y, 10)
+        self.assertEqual(rect.bottom_right.x, 5)
+        self.assertEqual(rect.bottom_right.y, 5)
 
     # Part 2
+    def test_shorter_duration_than_1(self):
+        self.assertTrue(hw2.shorter_duration_than(Duration(3, 30), Duration(4, 0)))
 
+    def test_shorter_duration_than_2(self):
+        self.assertFalse(hw2.shorter_duration_than(Duration(5, 0), Duration(4, 59)))
 
     # Part 3
+    def test_songs_shorter_than_1(self):
+        songs = [
+            Song("Artist A", "Song A", Duration(3, 30)),
+            Song("Artist B", "Song B", Duration(4, 0)),
+            Song("Artist C", "Song C", Duration(2, 45))
+        ]
+        result = hw2.songs_shorter_than(songs, Duration(4, 0))
 
+        expected = [
+            Song("Artist A", "Song A", Duration(3, 30)),
+            Song("Artist C", "Song C", Duration(2, 45))
+        ]
+        self.assertEqual(result, expected)
+
+    def test_songs_shorter_than_2(self):
+        songs = [
+            Song("Artist D", "Song D", Duration(5, 0)),
+            Song("Artist E", "Song E", Duration(4, 30)),
+            Song("Artist F", "Song F", Duration(4, 0))
+        ]
+        result = hw2.songs_shorter_than(songs, Duration(4, 0))
+
+        self.assertEqual(result, [])
 
     # Part 4
+    def test_running_time_1(self):
+        songs = [
+            Song("The Decemberists", "June Hymn", Duration(4, 30)),
+            Song("Broken Bells", "October", Duration(3, 40)),
+            Song("Kansas", "Dust in the Wind", Duration(3, 29)),
+            Song("Local Natives", "Airplanes", Duration(3, 58))
+        ]
+        playlist = [0, 2, 1, 3, 0]
+        result = hw2.running_time(songs, playlist)
+        expected_duration = Duration(20, 7)
+        self.assertEqual(result, expected_duration)
 
+    def test_running_time_2(self):
+        songs = [
+            Song("Artist X", "Song X", Duration(2, 45)),
+            Song("Artist Y", "Song Y", Duration(3, 15)),
+            Song("Artist Z", "Song Z", Duration(1, 55))
+        ]
+        playlist = [0, 1, 2]
+        result = hw2.running_time(songs, playlist)
+        expected_duration = Duration(7, 55)
+        self.assertEqual(result, expected_duration)
 
     # Part 5
+    def test_validate_route_1(self):
+        city_links = [
+            ['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']
+        ]
+        self.assertTrue(hw2.validate_route(city_links, ['san luis obispo', 'santa margarita', 'atascadero']))
 
+    def test_validate_route_2(self):
+        city_links = [
+            ['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']
+        ]
+        self.assertFalse(hw2.validate_route(city_links, ['san luis obispo', 'atascadero']))
 
     # Part 6
+    def test_longest_repetition_1(self):
+        self.assertEqual(hw2.longest_repetition([1, 1, 2, 2, 1, 1, 1, 3]), 4)
 
-
+    def test_longest_repetition_2(self):
+        self.assertEqual(hw2.longest_repetition([]), None)
 
 
 
